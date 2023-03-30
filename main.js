17434748e5b3be1107bfec68e0eeb6c666398e08
devaaamee <devaaamee@gmail.com>
2023-03-30 12:49:59 +0700

    sidebar toggle

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const toggleMenu = document.getElementById('toggle-menu')
+const toggleMenu = document.getElementById("toggle-menu");
+const sidebarMenu = document.getElementById("sidebar-menu");
 
-toggleMenu.addEventListener('click', () => {
-    const menuIcon = document.getElementById('menu-icon')
+toggleMenu.addEventListener("click", () => {
+  sidebarMenu.classList.toggle("show");
+  const menuIcon = document.getElementById("menu-icon");
 
-    menuIcon.classList.remove('fa-bars')
-    menuIcon.classList.add('fa-xmark')
+  let condition = menuIcon.classList.contains("fa-bars");
 
-})
+  if (condition) {
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+  } else {
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+  }
+});
